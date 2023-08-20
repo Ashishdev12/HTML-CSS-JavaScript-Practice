1d69336b336e4cf9b9e2b3bca398b59f0f625068
Ashish Kumar <myselfashish777@gmail.com>
2023-08-20 18:48:21 +0530

    updated

---
+++ Javascript-You-Tube/Part-3 Asynchronous JavaScript - 10/file11- XHR Request.js
@@ -0,0 +1,29 @@
+// XHR Request = 140
+
+const URL = "https://jsonplaceholder.typicode.com/posts";
+const xhr = new XMLHttpRequest();
+// console.log(xhr);
+// step 1
+// console.log(xhr.readyState);
+
+xhr.open("GET", URL);
+//  console.log(xhr.readyState);
+// xhr.onreadystatechange = function(){
+//   // console.log(xhr.readyState);
+//   if(xhr.readyState === 4){
+//     console.log(xhr);
+//     const response = xhr.response;
+//     const data = JSON.parse(response);
+//     // console.log(data);
+
+    
+//   }
+// }
+
+xhr.onload = function(){
+      const response = xhr.response;
+    const data = JSON.parse(response);
+}
+
+
+xhr.send();
